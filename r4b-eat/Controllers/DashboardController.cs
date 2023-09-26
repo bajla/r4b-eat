d01d96f85196821a38fcab00da10c9fe3ba6a846
VegasCool <aney.vaishnav5@gmail.com>
2023-09-26 10:47:06 +0200

    Fix me up lmoa

--- r4b-eat/Controllers/DashboardController.cs
+++ r4b-eat/Controllers/DashboardController.cs
@@ -3,14 +3,18 @@ using r4b_eat.Data;
 
 namespace r4b_eat.Controllers
 {
+<<<<<<< Updated upstream:r4b-eat/Controllers/DashboardController.cs
 	public class DashboardController
+=======
+	public class AdminController : Controller
+>>>>>>> Stashed changes:r4b-eat/Controllers/AdminController.cs
 	{
         private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext _db;
 
 
 
-        public DashboardController(ILogger<HomeController> logger, ApplicationDbContext db)
+        public AdminController(ILogger<HomeController> logger, ApplicationDbContext db)
 		{
             _logger = logger;
             _db = db;
