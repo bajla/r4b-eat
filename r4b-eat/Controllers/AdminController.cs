55e68fa4f95273a75c01dd144a43fe98cde3413c
VegasCool <aney.vaishnav5@gmail.com>
2023-10-03 10:13:37 +0200

    elca cakam

--- r4b-eat/Controllers/AdminController.cs
+++ r4b-eat/Controllers/AdminController.cs
@@ -41,8 +41,76 @@ namespace r4b_eat.Controllers
 
         public IActionResult Ucitelji()
         {
-            return View();
-        }
+            if (HttpContext.Session.GetString("userId") != null)
+            {
+
+                var query = from uporabnik in _db.uporabniki
+                            join poucevanje in _db.poucevanje
+                            on uporabnik.id_uporabnika equals poucevanje.id_uporabnika into joinedPoucevanje
+                            from leftJoinPoucevanje in joinedPoucevanje.DefaultIfEmpty()
+                            join predmet in _db.predmeti
+                            on leftJoinPoucevanje != null ? leftJoinPoucevanje.id_predmeta : (int?)null equals predmet.id_predmeta into joinedPredmet
+                            from leftJoinPredmet in joinedPredmet.DefaultIfEmpty()
+
+                            where uporabnik.pravice == "c"
+                            orderby uporabnik.ime ascending
+                            select new
+                            {
+                                
+                                uporabnik.ime,
+                                uporabnik.priimek,
+                                leftJoinPredmet.predmet,
+                                uporabnik.email
+
+                            };
+
+
+
+                var result = query.ToList();
+
+
+                List<ucenciDisplayModel> model = new List<ucenciDisplayModel>();
+
+                string lastIme = null;
+                string lastPriimek = null;
+                List<string> predmeti = new List<string>();
+                string email = "";
+                foreach (var i in result)
+                {
+                    if (lastIme == null)
+                    {
+                        lastIme = i.ime;
+                        lastPriimek = i.priimek;
+                        predmeti.Add(i.predmet);
+                        email = i.email;
+                    }
+
+                    else if (lastIme == i.ime)
+                    {
+                        predmeti.Add(i.predmet);
+
+                    }
+
+                    else if (lastIme != i.ime)
+                    {
+                        model.Add(new ucenciDisplayModel { predmeti = new List<string>(predmeti), ime = lastIme, email = email, priimek = lastPriimek });
+                        lastIme = i.ime;
+                        lastPriimek = i.priimek;
+                        email = i.email;
+                        predmeti.Clear();
+                        predmeti.Add(i.predmet);
+                    }
+                }
+
+                model.Add(new ucenciDisplayModel { predmeti = new List<string>(predmeti), ime = lastIme, email = email, priimek = lastPriimek });
+
+
+                return View(model);
+
+            }
+
+            return RedirectToAction("Index", "Home");
+    }
 
         public IActionResult Predmeti()
         {
@@ -115,17 +183,23 @@ namespace r4b_eat.Controllers
             if (HttpContext.Session.GetString("userId") != null)
             {
 
-                var query = from poucevanje in _db.poucevanje
-                            join uporabnik in _db.uporabniki on poucevanje.id_uporabnika equals uporabnik.id_uporabnika
-                            join predmet in _db.predmeti on poucevanje.id_predmeta equals predmet.id_predmeta
+                var query = from uporabnik in _db.uporabniki
+                            join poucevanje in _db.poucevanje
+                            on uporabnik.id_uporabnika equals poucevanje.id_uporabnika into joinedPoucevanje
+                            from leftJoinPoucevanje in joinedPoucevanje.DefaultIfEmpty()
+                            join predmet in _db.predmeti
+                            on leftJoinPoucevanje != null ? leftJoinPoucevanje.id_predmeta : (int?)null equals predmet.id_predmeta into joinedPredmet
+                            from leftJoinPredmet in joinedPredmet.DefaultIfEmpty()
+
                             where uporabnik.pravice == "u"
-                            orderby predmet.predmet ascending, uporabnik.ime ascending
+                            orderby uporabnik.ime ascending
                             select new
                             {
+                                
                                 uporabnik.ime,
                                 uporabnik.priimek,
-                                predmet.predmet,
-                                predmet.opis
+                                leftJoinPredmet.predmet,
+                                uporabnik.email
 
                             };
 
@@ -134,37 +208,40 @@ namespace r4b_eat.Controllers
                 var result = query.ToList();
 
 
-                List<predmetiDisplayModel> model = new List<predmetiDisplayModel>();
+                List<ucenciDisplayModel> model = new List<ucenciDisplayModel>();
 
-                string lastPredmet = null;
-                List<string> imena = new List<string>();
-                string opis = "";
+                string lastIme = null;
+                string lastPriimek = null;
+                List<string> predmeti = new List<string>();
+                string email = "";
                 foreach (var i in result)
                 {
-                    if (lastPredmet == null)
+                    if (lastIme == null)
                     {
-                        lastPredmet = i.predmet;
-                        imena.Add(i.ime + " " + i.priimek);
-                        opis = i.opis;
+                        lastIme = i.ime;
+                        lastPriimek = i.priimek;
+                        predmeti.Add(i.predmet);
+                        email = i.email;
                     }
 
-                    else if (lastPredmet == i.predmet)
+                    else if (lastIme == i.ime)
                     {
-                        imena.Add(i.ime + " " + i.priimek);
+                        predmeti.Add(i.predmet);
 
                     }
 
-                    else if (lastPredmet != i.predmet)
+                    else if (lastIme != i.ime)
                     {
-                        model.Add(new predmetiDisplayModel { ime = new List<string>(imena), predmet = lastPredmet, opis = opis });
-                        lastPredmet = i.predmet;
-                        opis = i.opis;
-                        imena.Clear();
-                        imena.Add(i.ime + " " + i.priimek);
+                        model.Add(new ucenciDisplayModel { predmeti = new List<string>(predmeti), ime = lastIme, email = email, priimek = lastPriimek });
+                        lastIme = i.ime;
+                        lastPriimek = i.priimek;
+                        email = i.email;
+                        predmeti.Clear();
+                        predmeti.Add(i.predmet);
                     }
                 }
 
-                model.Add(new predmetiDisplayModel { ime = new List<string>(imena), predmet = lastPredmet, opis = opis });
+                model.Add(new ucenciDisplayModel { predmeti = new List<string>(predmeti), ime = lastIme, email = email, priimek = lastPriimek });
 
 
                 return View(model);
@@ -183,6 +260,26 @@ namespace r4b_eat.Controllers
         {
             return View();
         }
+
+        public IActionResult AddUporabnik()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddUporabnik(uporabnikiEntity uporabnik)
+        {
+            if (HttpContext.Session.GetString("userId") != null)
+            {
+                
+                _db.uporabniki.Add(uporabnik);
+                _db.SaveChanges();
+
+            }
+
+            return View();
+        }
+
 
     }
 }
