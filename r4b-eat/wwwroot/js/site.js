9276bc04136a91cfae345a58007d81afcc2ce016
ElcMarin <eliostante123@gmail.com>
2023-10-03 09:13:22 +0200

    dijaki/admin add

--- r4b-eat/wwwroot/js/site.js
+++ r4b-eat/wwwroot/js/site.js
@@ -26,3 +26,21 @@ $(document).on('ready' function () {
   
 
 });
+
+// add uporabnik
+
+(function () {
+    'use strict'
+    const forms = document.querySelectorAll('.requires-validation')
+    Array.from(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
