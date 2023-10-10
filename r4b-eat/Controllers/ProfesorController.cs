dbc908b0e475fcc4c7f059a2abaebe1df50bc2bc
ElcMarin <eliostante123@gmail.com>
2023-10-10 09:26:53 +0200

    profesor/predmeti/predmet

---
+++ r4b-eat/Controllers/ProfesorController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using r4b_eat.Data;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using r4b_eat.Models;
+using r4b_eat.Data;
+
+
+
+namespace r4b_eat.Controllers
+{
+
+	public class ProfesorController : Controller
+	{
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
+
+
+
+        public ProfesorController(ILogger<HomeController> logger, ApplicationDbContext db)
+		{
+            _logger = logger;
+            _db = db;
+        }
+
+        public IActionResult Predmeti()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Profil()
+        {
+            return View();
+        }
+
+        public IActionResult Predmet()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
+
